f78b9b40fd82c3fe1a1dd8d6902969e67c120331
NNemanjaMM <nemanja.miladinovic@live.com>
2016-06-28 14:03:11 +0200

    sredjen izgled i raspored templ, dodata animacija

--- viewer/static/viewer/js/index.js
+++ viewer/static/viewer/js/index.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
     $("#cloud_select").val($("#cloud_select_helper").text());
+
+    $(".image-item").mouseenter(function(){
+        $(this).find(".over-image").slideDown();
+    });
+
+    $(".image-item").mouseleave(function(){
+        $(this).find(".over-image").slideUp();
+    });
 })
 
