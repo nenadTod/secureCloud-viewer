2e67cbe775cf7612b0904bb29e1decb84afeeb4b
NNemanjaMM <nemanja.miladinovic@live.com>
2016-06-28 14:41:34 +0200

    zapocinjem novu granu

--- viewer/static/viewer/js/index.js
+++ viewer/static/viewer/js/index.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
     $("#cloud_select").val($("#cloud_select_helper").text());
-})
 
+    $(".image-item").mouseenter(function(){
+        $(this).find(".over-image").slideDown();
+    });
+
+    $(".image-item").mouseleave(function(){
+        $(this).find(".over-image").slideUp();
+    });
+})
